f809e1d697b15ab121b05e7ac87744d0427b6c08
jbw <jbw@users.noreply.github.com>
2021-01-14 21:38:40 +0000

    Add project files.

---
+++ ActivitySourceExample/ActivityCreator.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace ActivitySourceExample
+{
+    public static class ActivityCreator
+    {
+        public static string ActivitySourceName = typeof(ActivityCreator).Assembly.GetName().Name;
+        public static string ActivitySourceVersion = typeof(ActivityCreator).Assembly.GetName().Version.ToString();
+        private static string ActivityName = ActivitySourceName + ".Execute";
+
+        public static void CreateActivity()
+        {
+            Task.Run(() =>
+            {
+                using var activitySource = new ActivitySource(ActivitySourceName, ActivitySourceVersion);
+
+                var activity = StartActivity(activitySource);
+
+                PrintActivity(activity);
+
+                StopActivity(activity);
+
+            });
+        }
+
+        static Activity StartActivity(ActivitySource source)
+        {
+            var activity = source.StartActivity(ActivityName, ActivityKind.Client);
+
+            activity.SetParentId(Guid.NewGuid().ToString());
+
+            activity.Start();
+
+            return activity;
+        }
+
+        static void StopActivity(Activity activity)
+        {
+            activity.SetEndTime(DateTime.UtcNow);
+
+            activity.Stop();
+        }
+
+        static void PrintActivity(Activity activity)
+        {
+            Console.WriteLine("================= Activity Info ===========================");
+            Console.WriteLine(activity.OperationName);
+            Console.WriteLine(activity.Id);
+            Console.WriteLine(activity.TraceStateString);
+            Console.WriteLine(activity.ParentSpanId);
+            Console.WriteLine(activity.TraceId);
+            Console.WriteLine(activity.SpanId);
+            Console.WriteLine("===========================================================");
+            Console.WriteLine();
+
+        }
+
+    }
+}
---
+++ ActivitySourceExample/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+using ActivitySourceExample;
+using OpenTelemetry;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
+
+Activity.DefaultIdFormat = ActivityIdFormat.W3C;
+
+// Build the trace provider using OpenTelementry.
+// Export activity to console and Jaeger.
+
+// See https://www.jaegertracing.io/docs/1.21/getting-started/ and run Jaeger 
+// in a Docker comtainer.
+
+using var openTelemetry = Sdk.CreateTracerProviderBuilder()
+    .AddSource(ActivityCreator.ActivitySourceName)
+    .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService("ActivitySourceExample.Console"))
+    .AddJaegerExporter()
+    .AddConsoleExporter()
+    .Build();
+
+// Created activity will be exported to console and Jaeger.
+ActivityCreator.CreateActivity();
+
+Console.ReadLine();
